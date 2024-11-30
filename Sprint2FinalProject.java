c9354a6dad1bd88b9a0bb291cb1854b3b5a521f6
Omar El Bakir El Barazi <65897857+OmarElBarazi@users.noreply.github.com>
2024-11-30 12:11:31 -0800

    Update Sprint2FinalProject.java

--- Sprint2FinalProject.java
+++ Sprint2FinalProject.java
@@ -1,4 +1,3 @@
-
 import java.math.BigDecimal;
 import java.util.Stack;
 import org.json.JSONObject;
@@ -6,51 +5,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
 import java.util.Arrays;
 
 //TODO: create a "RATE" interface with a function getRate() that returns a BigDecimal.
+
+
+
+
+
+
+
+
+
+
+
+ 
 //TODO: create a BigDecimalRate class that uses the Rate interface
 //it should practice data encapsulation, set the rate variable of datatype BigDecimal, 
 //and practice data encapsulation
+
+
+
+
+
+
+
+
+
+
+
+ 
 //TODO: create a NumberRate class that uses the Rate interface
 //it should practice data encapsulation, and the getRate function has already been created for you
 //be sure your constructor sets the rate variable.
-interface Rate {
-
-    BigDecimal getRate();
-}
-
-class BigDecimalRate implements Rate {
-
-    private BigDecimal rate;
-
-    public BigDecimalRate(BigDecimal rate) {
-        this.rate = rate;
-    }
-
-    public BigDecimal getRate() {
-        return rate;
-    }
-}
-
-class NumberRate implements Rate {
-
-    private Number rate;
-
-    public NumberRate(Number rate) {
-        this.rate = rate;
-    }
 
     public BigDecimal getRate() {
         return BigDecimal.valueOf(rate.doubleValue());
     }
-}
 
+
+
+
+
+
+
+
+
+
+
+ 
 // Class representing a currency path
 class CurrencyPath {
-
     private Stack<String> path;
     private BigDecimal runningTotal;
     private Object[][] allRates;
@@ -71,29 +77,32 @@ class CurrencyPath {
 
     //TODO: create getter functions for path, listOfCurrencies, and allRates
 
-     // Getter for the path
-     public Stack<String> getPath() {
-        return path;
-    }
 
-    // Getter for the list of currencies
-    public String[] getListOfCurrencies() {
-        return listOfCurrencies;
-    }
 
-    // Getter for all rates
-    public Object[][] getAllRates() {
-        return allRates;
-    }
 
-    
+
+
+
+
+
+
+
+ 
     // Recursive function to find the optimal path
     private void recurse(int depth, String start) {
         if (depth == 0) {
             // Base case: reached the desired depth
             //TODO: push start to the path Stack variable and return
-            path.push(start);
-            return;
+
+
+
+
+
+
+
+
+
+
 
         }
 
@@ -117,9 +126,16 @@ class CurrencyPath {
         }
 
         //TODO: push start to path variable and set runningTotal to bestValue
+
+
+
+
+
+
+
+
+
         // Recursive call with updated values
-        path.push(start);
-        runningTotal=bestValue;
         recurse(depth - 1, bestNextCurrency);
     }
 
@@ -150,7 +166,18 @@ public class Sprint2FinalProject {
         while (!path.isEmpty()) {
 
             //create a variable called nextCurrency set it to top value on the path Stack. be sure to pop.
-            String nextCurrency = path.pop();
+
+
+
+
+
+
+
+
+
+
+
+
             int currentIndex = Arrays.asList(currencyPath.getListOfCurrencies()).indexOf(currentCurrency);
             int nextIndex = Arrays.asList(currencyPath.getListOfCurrencies()).indexOf(nextCurrency);
 
@@ -176,47 +203,20 @@ public class Sprint2FinalProject {
     }
 
     // Connect to the API and retrieve the initial data
-    // Connect to the API and retrieve the initial data
     public static JSONObject connect(String apiAddress) {
-        try {
-            // Create URL object with the API address
-            URL url = new URL(apiAddress);
+        //TODO: make this function return a JSONObject, created from a String retrieved from the apiAddress
 
-            // Open a connection to the URL
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            // Set the request method to GET
-            connection.setRequestMethod("GET");
 
-            // Set the timeout for the connection
-            connection.setConnectTimeout(5000); // 5 seconds
 
-            // Check if the request was successful (HTTP status 200)
-            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                // Read the response
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                StringBuilder response = new StringBuilder();
 
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-                reader.close();
 
-                // Parse the JSON response
-                JSONObject jsonResponseObject = new JSONObject(response.toString());
-                return jsonResponseObject;
-            } else {
-                // Handle error response
-                System.out.println("Error: " + connection.getResponseCode() + " " + connection.getResponseMessage());
-            }
-            // Close the connection
-            connection.disconnect();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+
+
+
+
+ 
     }
 
     // Main method to run the project
@@ -227,13 +227,21 @@ public class Sprint2FinalProject {
 
         //TODO: connect() to the apiAddress above and get the rates info from that address as a JSONObject
         //store it in a variable called "rates"
-        JSONObject jsonResponseObject = connect(apiAddress);
 
-        // Extract information from the JSON
-        JSONObject rates = jsonResponseObject.getJSONObject("rates");
+
+
+
+
+
+
+
+
+
+
+        
+        // Extract information from JSONObject rates variable
         String[] keys = new String[rates.keySet().size()];
         keys = rates.keySet().toArray(keys);
-
 
         Object[][] allRates = new Object[keys.length][keys.length];
         for (int i = 0; i < keys.length; i++) {
@@ -247,9 +255,17 @@ public class Sprint2FinalProject {
         //TODO: set the String start and int depth values by user input
         //create an instance of CurrencyPath with allRates and keys
         //Run calculateResult with appropriate parameters.
-        String start = "USD";
-        int depth = 1000;
-        CurrencyPath currencyPath = new CurrencyPath(allRates, keys);
-        calculateResult(currencyPath, start, depth);
+
+
+
+
+
+
+
+
+
+
+
+ 
     }
 }
